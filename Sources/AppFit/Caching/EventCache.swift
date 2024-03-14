6aded89cd83f52ad90ac57127f2c1fed9556261b
Anthony Castelli <mranthonycastelli@gmail.com>
2024-03-14 18:11:22 -0400

    Remove on-disk caching for now
    This removes all of the on-disk caching for now. More thuoght needs to go into how we truely need this to function.

--- Sources/AppFit/Caching/EventCache.swift
+++ Sources/AppFit/Caching/EventCache.swift
@@ -13,12 +13,6 @@ import Foundation
 internal actor EventCache {
     private var cache: [String: AppFitEvent] = [:]
 
-    internal init() {
-        Task.detached {
-            await self.readDataFromDisk()
-        }
-    }
-
     /**
      * The events that have been cached.
      */
@@ -33,7 +27,6 @@ internal actor EventCache {
      */
     internal func add(event: AppFitEvent) {
         self.cache[event.id.uuidString] = event
-        self.saveDataToDisk()
     }
 
     /**
@@ -43,7 +36,6 @@ internal actor EventCache {
      */
     internal func remove(id: String) {
         self.cache.removeValue(forKey: id)
-        self.saveDataToDisk()
     }
 
     /**
@@ -53,7 +45,6 @@ internal actor EventCache {
      */
     internal func remove(event: AppFitEvent) {
         self.remove(id: event.id.uuidString)
-        self.saveDataToDisk()
     }
 
     /**
@@ -61,46 +52,5 @@ internal actor EventCache {
      */
     internal func clear() {
         self.cache.removeAll()
-        self.saveDataToDisk()
-    }
-}
-
-extension EventCache {
-    /// Reads the data from disk
-    private func readDataFromDisk() {
-        do {
-            let data = try Data(contentsOf: self.cachePath())
-            let decoder = JSONDecoder()
-            let cache = try decoder.decode([AppFitEvent].self, from: data)
-            self.cache = Dictionary(uniqueKeysWithValues: cache.map({ ($0.id.uuidString, $0) }))
-        } catch {
-            print("[EventCache] Error reading cache: \(error)")
-        }
-    }
-
-    /// Writes the data to disk
-    private func saveDataToDisk() {
-        do {
-            let encoder = JSONEncoder()
-            let data = try encoder.encode(self.events)
-            try data.write(to: self.cachePath(), options: [.atomic])
-        } catch {
-            print("[EventCache] Error writing cache \(error)")
-        }
-    }
-
-    /// Helper method for building out the path directory
-    func cachePath() throws -> URL {
-        if #available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *) {
-            let documents =  URL.documentsDirectory.appending(component: "appfit")
-            try FileManager.default.createDirectory(at: documents, withIntermediateDirectories: true)
-            return documents.appending(component: "cache.af")
-        } else {
-            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            let documentsDirectory = paths.first!
-            let cacheFolder = documentsDirectory.appendingPathComponent("appfit")
-            try FileManager.default.createDirectory(at: cacheFolder, withIntermediateDirectories: true)
-            return cacheFolder.appendingPathComponent("cache.af")
-        }
     }
 }
