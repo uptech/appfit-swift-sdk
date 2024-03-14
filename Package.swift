390ad989db6b13a0e01ec5aa9a144fc01d247ca3
Anthony Castelli <mranthonycastelli@gmail.com>
2024-03-14 15:44:24 -0400

    Add support for specific OS Releases
    This adds support for specific os releases. This ensures that we support a certain version of swift allowing us to utilize the async features for thread saftey.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "AppFit",
+    platforms: [
+        .macOS(.v10_15),
+        .macCatalyst(.v13),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+        .visionOS(.v1)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "AppFit", targets: ["AppFit"]),
