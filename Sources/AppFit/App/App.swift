344e78c24b43ba5617e66095d9d8b67c0e95be46
Anthony Castelli <mranthonycastelli@gmail.com>
2024-06-18 08:24:36 -0400

    Add Support for CFBundleShortVersionString
    This adds support for the `CFBundleShortVersionString` as well as the build. We really only care about the `CFBundleShortVersionString` for now, but both are available if needed.

--- Sources/AppFit/App/App.swift
+++ Sources/AppFit/App/App.swift
@@ -11,6 +11,10 @@ internal struct App {
     static let current = App()
 
     var version: String? {
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    }
+
+    var build: String? {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
     }
 }
